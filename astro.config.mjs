abc9b089c5a267b7e272e248f9feaaf733cf21dd
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2025-06-09 17:13:18 -0400

    Iconset and font implentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Quicksand",
+        cssVariable: "--font-quicksand",
+      },
+    ],
+  },
 });
