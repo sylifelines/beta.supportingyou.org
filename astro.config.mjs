10eaeaa737bb6995fea4540670cb6eaadccf34e3
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2025-06-28 16:15:54 -0400

    Implement the WhatTodDo section

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,32 +9,36 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    icon({
-      include: {
-        // Include only required stash icons in the bundle
-        "material-symbols": [
-          "menu-rounded",
-          "account-circle-outline",
-          "shopping-cart-outline",
-        ],
-        // Include all `uis` icons
-        // uis: ['*']
-      },
-    }),
-    alpinejs(),
-  ],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.google(),
-        name: "Quicksand",
-        cssVariable: "--font-quicksand",
-      },
+    integrations: [
+        icon({
+            include: {
+                // Include only required stash icons in the bundle
+                "material-symbols": [
+                    "menu-rounded",
+                    "account-circle-outline",
+                    "shopping-cart-outline",
+                    "network-node",
+                    "emoji-people-rounded",
+                    "voice-chat-outline-rounded",
+                    "kid-star-outline",
+                ],
+                // Include all `uis` icons
+                // uis: ['*']
+            },
+        }),
+        alpinejs(),
     ],
-  },
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    experimental: {
+        fonts: [
+            {
+                provider: fontProviders.google(),
+                name: "Quicksand",
+                cssVariable: "--font-quicksand",
+            },
+        ],
+    },
 });
