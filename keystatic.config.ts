12965b2bff2952c3221441948f15cf475362fbc7
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2025-07-22 17:40:02 -0400

    Continue with Keystatic integration

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,13 +1,18 @@
 import { config, fields, singleton } from "@keystatic/core";
-
+import { nanoid } from 'nanoid';
 import icons from "./src/data/icons.json";
+
 const {
   icons: { "material-symbols": materialSymbols },
 } = icons;
 
 export default config({
   storage: {
-    kind: "local",
+    kind: 'local',
+    // kind: 'cloud',
+  },
+  cloud: {
+    project: 'sylifelines/supportingyou',
   },
   singletons: {
     hero: singleton({
@@ -15,13 +20,9 @@ export default config({
       path: "./src/data/hero",
       format: "json",
       schema: {
-        // items: fields.array(
-        // data:    fields.object({
         heading: fields.text({ label: "Heading" }),
         subheading: fields.text({ label: "Sub Heading" }),
         buttonText: fields.text({ label: "Button Text" }),
-        // }),
-        // ),
       },
     }), // hero end
     community: singleton({
@@ -40,7 +41,7 @@ export default config({
           {
             label: "Lifelines",
             slugField: "title",
-            itemLabel: (props) => props.fields.title.value,
+            itemLabel: props => props.fields.title.value,
           },
         ),
       },
@@ -63,7 +64,53 @@ export default config({
               defaultValue: `material-symbols:${materialSymbols[0]}`,
             }),
             title: fields.text({ label: "Title" }),
+            id: fields.text({ label: "Automatic ID - JUST IGNORE", defaultValue: nanoid() }),
           }),
+          {
+              label: "What To Dos",
+              slugField: "id",
+              itemLabel: props => props.fields.title.value,
+          }
+        ),
+      },
+    }),
+    businessBenefits: singleton({
+      label: "Business Benefits Section",
+      path: "src/data/businessBenefits",
+      format: "json",
+      schema: {
+        heading: fields.text({ label: "Heading" }),
+        subheading: fields.text({ label: "Sub Heading" }),
+        items: fields.array(
+          fields.object({
+            title: fields.text({ label: "Title" }),
+            description: fields.text({ label: "Description" }),
+          }),
+          {
+              label: "Benefits",
+              slugField: "title",
+              itemLabel: props => props.fields.title.value,
+          }
+        ),
+      },
+    }),
+    whatNext: singleton({
+      label: "What Next Section",
+      path: "src/data/whatNext",
+      format: "json",
+      schema: {
+        heading: fields.text({ label: "Heading" }),
+        subheading: fields.text({ label: "Sub Heading" }),
+        buttonText: fields.text({ label: "Buttin Text" }),
+        items: fields.array(
+          fields.object({
+            text: fields.text({ label: "Title" }),
+          }),
+          {
+              label: "Benefits",
+              slugField: "text",
+              itemLabel: props => props.fields.text.value,
+          }
         ),
       },
     }),
@@ -77,8 +124,9 @@ export default config({
             fields.text({ label: "Tag" }),
             // Labelling options
             {
-              label: "Material Icon",
-              itemLabel: (props) => props.value,
+              label: "Material Icons for use on the site",
+              description: "[Material Icons](https://icon-sets.iconify.design/material-symbols/)",
+              itemLabel: props => props.value,
             },
           ),
         }),
